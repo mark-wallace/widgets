774709da5bb52847ec626d0ad9cee40e9d44f983
Mark Wallace <mark.wallace6@gmail.com>
2021-01-10 11:50:51 -0600

    css styling, accordion dev

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-    return <h1>{items.length}</h1>;
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const titleOnClick = (index) => {
+        console.log("Title index clicked " + index);
+        setActiveIndex(index);
+    }
+
+    const renderedItems = items.map((item, index) => {
+        return(
+            <React.Fragment key={item.title}>
+                <div className="title active" onClick={() => titleOnClick(index)}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{ item.content }</p>
+                </div>
+            </React.Fragment>
+        )
+    })
+    
+    return <div className="ui styled accordion">
+        {renderedItems}
+        <div>{activeIndex}</div>
+    </div>
 }
 
 export default Accordion;
