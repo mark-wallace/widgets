025102e34bc3ed26663bbc6e0f734e14721b2920
Mark Wallace <mark.wallace6@gmail.com>
2021-01-24 10:23:44 -0600

    add wikipedia search and rendered results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,41 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('world of warcraft');
+    const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        //sync cannot be used with useEffect, so this helper neeeds to exist
+       const search = async () => {
+           const { data } = await axios.get('https://en.wikipedia.org/w/api.php',
+           {
+               params: {
+                    action: "query",
+                    list: "search",
+                    format: "json",
+                    origin: "*",
+                    srsearch: term,
+               }
+           });
+           setResults(data.query.search);
+       };
+       search();
+    }, [term])
+
+    const renderedResults = results.map((result) => {
+        return(
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+        )
+    })
     return(
         <div>
             <div className="ui form">
@@ -15,6 +48,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     )
 }
