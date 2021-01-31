43cdf0bc7c16097128968745c82e166473284eee
Mark Wallace <mark.wallace6@gmail.com>
2021-01-31 11:30:44 -0600

    finished accordion. adding dropdown

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,75 +1,74 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState('world of warcraft');
-    const [debouncedTerm, setDebouncedTerm] = useState(term);
-    const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("world of warcraft");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-            setDebouncedTerm(term);
-        }, 1000)
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-        return () => {
-            clearTimeout(timerId);
-        }
-    } ,[term])
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
-    useEffect(() => {
-        //sync cannot be used with useEffect, so this helper neeeds to exist
-       const search = async () => {
-           const { data } = await axios.get('https://en.wikipedia.org/w/api.php',
-           {
-               params: {
-                    action: "query",
-                    list: "search",
-                    format: "json",
-                    origin: "*",
-                    srsearch: debouncedTerm,
-               }
-           });
-           setResults(data.query.search);
-       };
-        if (debouncedTerm) {
-            search();
-        }
-    }, [debouncedTerm])
+  useEffect(() => {
+    //sync cannot be used with useEffect, so this helper neeeds to exist
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          format: "json",
+          origin: "*",
+          srsearch: debouncedTerm,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (debouncedTerm) {
+      search();
+    }
+  }, [debouncedTerm]);
 
-    const renderedResults = results.map((result) => {
-        return(
-            <div key={result.pageid} className="item">
-                <div className="right floated content">
-                    <a 
-                        className="button ui"
-                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
-                    >Go</a>
-                </div>
-                <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    {result.snippet}
-                </div>
-            </div>
-        )
-    })
-    return(
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input 
-                        value={term}
-                        onChange={(e) => setTerm(e.target.value)}
-                        className="input"
-                    />
-                </div>
-            </div>
-            <div className="ui celled list">{renderedResults}</div>
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="button ui"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
-    )
-}
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
 
 export default Search;
